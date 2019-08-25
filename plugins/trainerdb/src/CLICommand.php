3167db4a1ef4d0ba453c97b0a1314871d556f6d1
Evan Hildreth <evan.hildreth@webdevstudios.com>
2019-08-25 17:01:06 -0400

    Hitting TCGP's API!

--- plugins/trainerdb/src/CLICommand.php
+++ plugins/trainerdb/src/CLICommand.php
@@ -107,4 +107,51 @@ class CLICommand extends \WP_CLI_Command {
 		}
 		\WP_CLI::success( 'Cards imported!' );
 	}
+
+	public function tcgp() {
+		$response = wp_remote_get(
+			'http://api.tcgplayer.com/v1.32.0/catalog/products?categoryId=3&productTypes=Cards&groupId=2377&getExtendedFields=true&includeSkus=true&offset=0&limit=200',
+			[
+				'headers' => [
+					'Authorization' => 'Bearer ' . TCGPLAYER_ACCESS_TOKEN,
+					'Accept'        => 'application/json',
+					'Content-Type'  => 'application/json',
+				],
+			]
+		);
+
+		$api_response = json_decode( $response['body'] );
+		$cards        = $api_response->results;
+
+		foreach ( $cards as $card ) {
+			$card_number = 0;
+			foreach ( $card->extendedData as $edat ) {
+				if ( 'Number' === $edat->name ) {
+					$card_number = $edat->value;
+					break;
+				}
+			}
+
+			foreach ( $card->skus as $sku ) {
+				if ( 1 === $sku->languageId && 1 === $sku->conditionId ) {
+					$args = [
+						'post_type'   => 'card',
+						'post_status' => 'draft',
+						'meta_input'  => [
+							'card_number'         => $card_number,
+							'ptcg_id'             => 'sm9-' . $card_number,
+							'tcgp_id'             => $sku->skuId,
+							'reverse_holographic' => ( 77 === $sku->printingId ),
+						],
+					];
+
+					$result = wp_insert_post( $args, true );
+					if ( is_wp_error( $result ) ) {
+						\WP_CLI::error( $result->get_error_message() );
+					}
+					\WP_CLI::success( 'sm9-' . $card_number . ' imported.' );
+				}
+			}
+		}
+	}
 }
