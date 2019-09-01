2393f8296bc289f392923931240fe8fa6251d76a
Evan Hildreth <evan.hildreth@webdevstudios.com>
2019-09-01 16:23:24 -0400

    Changing parent theme to wp-bootstratp-starter

---
+++ themes/trainerdb/functions.php
@@ -0,0 +1,6 @@
+<?php
+
+function my_theme_enqueue_styles() {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
