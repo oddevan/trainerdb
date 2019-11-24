0c9f5fc3a98ffe872794159100574570242eb660
Evan Hildreth <evan.hildreth@webdevstudios.com>
2019-11-23 21:39:31 -0500

    Scaffold out sub models

---
+++ plugins/trainerdb/src/Model/Ability.php
@@ -0,0 +1,17 @@
+<?php //phpcs:ignore Wordpress.Files.Filename
+/**
+ * Class to model the Ability structure
+ *
+ * @since 0.1.0
+ * @package oddEvan\TrainerDB
+ */
+
+namespace oddEvan\TrainerDB\Model;
+
+/**
+ * Class to model the Ability structure
+ *
+ * @since 0.1.0
+ */
+class Ability {
+}
---
+++ plugins/trainerdb/src/Model/Attack.php
@@ -0,0 +1,17 @@
+<?php //phpcs:ignore Wordpress.Files.Filename
+/**
+ * Class to model the Attack structure
+ *
+ * @since 0.1.0
+ * @package oddEvan\TrainerDB
+ */
+
+namespace oddEvan\TrainerDB\Model;
+
+/**
+ * Class to model the Attack structure
+ *
+ * @since 0.1.0
+ */
+class Attack {
+}
