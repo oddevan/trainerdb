433251d95fb81e7a184e0ca127293be0ae83e741
Evan Hildreth <slightly@oddevan.com>
2019-12-31 19:00:31 -0500

    Documentaion for Attack

--- plugins/trainerdb/src/Model/Attack.php
+++ plugins/trainerdb/src/Model/Attack.php
@@ -8,18 +8,45 @@
 
 namespace oddEvan\TrainerDB\Model;
 
-use oddEvan\TrainerDB\Import\TcgPlayerHelper;
-
 /**
  * Class to model the Attack structure
  *
  * @since 0.1.0
  */
 class Attack {
+
+	/**
+	 * Stores the raw text of the attack
+	 *
+	 * @since 0.1.0
+	 * @var string $raw
+	 */
 	private $raw = '';
+
+	/**
+	 * Stores the results of `preg_match`, typically the cost, name, and damage of the attack.
+	 *
+	 * @since 0.1.0
+	 * @var array $match_results
+	 */
 	private $match_results = [];
+
+	/**
+	 * Stores any additional rules text for the attack
+	 *
+	 * @since 0.1.0
+	 * @var string $text
+	 */
 	private $text = '';
 
+	/**
+	 * Construct an Attack object from the text from TCGPlayer.
+	 *
+	 * @since 0.1.0
+	 * @author Evan Hildreth <me@eph.me>
+	 *
+	 * @param string $raw_text Text from TCGPlayer's API to be parsed.
+	 */
 	public function __construct( $raw_text ) {
 		$this->raw     = $raw_text;
 		$stripped_text = wp_strip_all_tags( $raw_text );
@@ -31,22 +58,63 @@ class Attack {
 		}
 	}
 
+	/**
+	 * Energy cost of this attack. TODO: refactor to use EnergyType
+	 *
+	 * @since 0.1.0
+	 * @author Evan Hildreth <me@eph.me>
+	 *
+	 * @return string Energy cost for this attack.
+	 */
 	public function get_cost() {
 		return $this->match_results[1] ?? 0;
 	}
 
-	public function get_name() {
+	/**
+	 * Name of this attack
+	 *
+	 * @since 0.1.0
+	 * @author Evan Hildreth <me@eph.me>
+	 *
+	 * @return string Name of this attack.
+	 */
+	public function get_name() : string {
 		return $this->match_results[2] ?? '';
 	}
 
-	public function get_base_damage() {
+	/**
+	 * Base damage for this attack. Does not take into account multipliers
+	 * or additions. Status-only attacks return 0.
+	 *
+	 * @since 0.1.0
+	 * @author Evan Hildreth <me@eph.me>
+	 *
+	 * @return int Base damage for this attack; 0 if not applicable.
+	 */
+	public function get_base_damage() : int {
 		return $this->match_results[5] ?? 0;
 	}
 
-	public function get_text() {
+	/**
+	 * Extra rules text for the attack. Empty string if not applicable.
+	 *
+	 * @since 0.1.0
+	 * @author Evan Hildreth <me@eph.me>
+	 *
+	 * @return string Rules text for attack; empty if not applicable.
+	 */
+	public function get_text() : string {
 		return $this->text;
 	}
 
+	/**
+	 * Attack information as associative array for an update/insert post.
+	 *
+	 * @since 0.1.0
+	 * @author Evan Hildreth <me@eph.me>
+	 *
+	 * @return array Metadata arguments for this attack in associative array.
+	 */
 	public function get_post_args() {
 		return [
 			'cost'        => $this->get_cost(),
