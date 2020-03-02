3f722624631439d4f706ad392d8816aaa54fc6be
Evan Hildreth <slightly@oddevan.com>
2020-03-01 21:49:16 -0500

    Scaffold LibraryHelper class

---
+++ plugins/trainerdb/src/Database/LibraryHelper.php
@@ -0,0 +1,20 @@
+<?php //phpcs:ignore Wordpress.Files.Filename
+/**
+ * Class LibrayHelper for TrainerDB
+ *
+ * @since 0.1.0
+ * @package oddEvan\TrainerDB
+ */
+
+namespace oddEvan\TrainerDB\Database;
+
+/**
+ * Class LibraryHelper
+ *
+ * Methods for interacting with the Library custom table
+ *
+ * @since 0.1.0
+ */
+class LibraryHelper {
+	
+}
