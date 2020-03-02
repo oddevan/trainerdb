20b57735e9cb97582397eaf0b3f43a65d1dd59c1
Evan Hildreth <slightly@oddevan.com>
2020-03-01 21:49:55 -0500

    Scaffold out Add method

--- plugins/trainerdb/src/Endpoint/Library/Add.php
+++ plugins/trainerdb/src/Endpoint/Library/Add.php
@@ -9,6 +9,7 @@
 namespace oddEvan\TrainerDB\Endpoint\Library;
 
 use WebDevStudios\OopsWP\Structure\Content\ApiEndpoint;
+use oddEvan\TrainerDB\Database\LibraryHelper;
 
 /**
  * Registrar class to register our custom post types
@@ -16,11 +17,17 @@ use WebDevStudios\OopsWP\Structure\Content\ApiEndpoint;
  * @since 0.1.0
  */
 class Add extends ApiEndpoint {
+	private $library_helper = null;
+
+	public function __construct() {
+		$this->library_helper = new LibraryHelper();
+	}
+
 	/**
 	 * The endpoint namespace
 	 *
 	 * @var string
-	 * @since 2020-01-30
+	 * @since  0.1.0
 	 */
 	protected $namespace = 'trainerdb/v1';
 
@@ -28,16 +35,32 @@ class Add extends ApiEndpoint {
 	 * The endpoint route
 	 *
 	 * @var string
-	 * @since 2020-01-30
+	 * @since  0.1.0
 	 */
 	protected $route = '/library/add';
+
+	/**
+	 * Get the arguments for this post type.
+	 *
+	 * Extending classes can override this method to pass in their own customizations specific to the endpoint.
+	 *
+	 * @author Evan Hildreth <evan.hildreth@webdevstudios.com>
+	 * @since  0.1.0
+	 * @return array
+	 */
+	protected function get_args() : array {
+		return [
+			'methods' => \WP_REST_Server::CREATABLE,
+		];
+	}
 
 	/**
 	 * This is our callback function that embeds our phrase in a WP_REST_Response
 	 *
 	 * @return WP_REST_Response
 	 */
-	public function run() {
-		return rest_ensure_response( 'Hello World, this is the WordPress REST API' );
+	public function run( $request = null ) {
+		$data = $request->get_json_params();
+		return new \WP_REST_Response( $data, 200 );
 	}
 }
