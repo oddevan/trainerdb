45b7edaa39b07537aa92f7be80cef3fe9893a128
Evan Hildreth <slightly@oddevan.com>
2020-03-10 21:55:49 -0400

    Preparing for React

---
+++ plugins/trainerdb/src/Endpoint/Card/Search.php
@@ -0,0 +1,67 @@
+<?php //phpcs:ignore Wordpress.Files.Filename
+/**
+ * Content Registrar for the plugin
+ *
+ * @since 0.1.0
+ * @package oddEvan\TrainerDB
+ */
+
+namespace oddEvan\TrainerDB\Endpoint\Library;
+
+use WebDevStudios\OopsWP\Structure\Content\ApiEndpoint;
+
+/**
+ * Registrar class to register our custom post types
+ *
+ * @since 0.1.0
+ */
+class Add extends ApiEndpoint {
+	/**
+	 * The endpoint namespace
+	 *
+	 * @var string
+	 * @since  0.1.0
+	 */
+	protected $namespace = 'trainerdb/v1';
+
+	/**
+	 * The endpoint route
+	 *
+	 * @var string
+	 * @since  0.1.0
+	 */
+	protected $route = '/card/search/(?P<id>[a-zA-Z0-9]+)';
+
+	/**
+	 * Get the arguments for this post type.
+	 *
+	 * Extending classes can override this method to pass in their own customizations specific to the endpoint.
+	 *
+	 * @author Evan Hildreth <evan.hildreth@webdevstudios.com>
+	 * @since  0.1.0
+	 * @return array
+	 */
+	protected function get_args() : array {
+		return [
+			'methods' => \WP_REST_Server::READABLE,
+		];
+	}
+
+	/**
+	 * This is our callback function that embeds our phrase in a WP_REST_Response
+	 *
+	 * @return WP_REST_Response
+	 */
+	public function run( $request = null ) {
+		$data    = $request->get_json_params();
+		$results = [];
+
+		if ( isset( $data['cards'] ) && is_array( $data['cards'] ) ) {
+			foreach ( $data['cards'] as $card_id => $quantity ) {
+				$new_quantity        = $this->library_helper->adjust_quantity( 1, $card_id, $quantity );
+				$results[ $card_id ] = $new_quantity;
+			}
+		}
+		return new \WP_REST_Response( $results, 200 );
+	}
+}
